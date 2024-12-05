c167003663986664822d6bec76156bd9c76a1cfd
Haneen Abumazrou <148560961+HaneenAbumazrou@users.noreply.github.com>
2024-12-05 13:25:30 +0300

    search route

--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,6 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-Route::get('/movies/search', [MovieController::class, 'search']);
+
 
 require __DIR__ . '/auth.php';
