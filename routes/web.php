c16383bce7b297df4802417a0307cf34809e406f
Haneen Abumazrou <148560961+HaneenAbumazrou@users.noreply.github.com>
2024-12-05 09:37:22 +0300

    search

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\MovieController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -36,5 +37,6 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+Route::get('/movies/search', [MovieController::class, 'search']);
 
 require __DIR__ . '/auth.php';
