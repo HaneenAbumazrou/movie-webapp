7b98ccdb095cb48356e6dc220853d4e4fd867c91
reemsweity <reemsweity826@gmail.com>
2024-12-06 21:36:51 +0300

    update-about-us

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,7 @@ Route::get('/', function () {
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
     ]);
-});
+})->name('welcome');
 Route::get('/about-us', function () {
     return Inertia::render('about-us');
 })->name('about-us');
